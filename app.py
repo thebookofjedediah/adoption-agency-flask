2b2d470f4070cecef4ad8963177b24035f6e3227
Jedediah Arnold <jarnold910@gmail.com>
2020-08-31 13:56:05 -0400

    adding a pet form working

--- app.py
+++ app.py
@@ -21,8 +21,32 @@ def get_home():
     pets = Pet.query.all()
     return render_template('home.html', pets=pets)
 
+# SHOW PET DETAILS
 @app.route('/<int:pet_id>')
 def show_pet_details(pet_id):
     """show details about a single pet"""
     pet = Pet.query.get_or_404(pet_id)
     return render_template('pet-details.html', pet=pet)
+
+# CREATE NEW PET FORM
+@app.route('/add')
+def add_pet_form():
+    """form to create a new pet"""
+    return render_template('pet-form.html')
+
+# CREATE THE NEW PET
+@app.route('/add', methods=['POST'])
+def create_new_pet():
+    """Creating a new pet and go to home page"""
+    name = request.form["name"]
+    species = request.form["species"]
+    photo_url = request.form["photo_url"]
+    age = request.form["age"]
+    notes = request.form["notes"]
+    age = int(age) if age else None
+
+    new_pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes)
+    db.session.add(new_pet)
+    db.session.commit()
+    
+    return redirect('/')
